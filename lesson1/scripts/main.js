0257dff283edaacd8147e9ff1d10569fdc79a609
GrayHead <ss.zhuravlov@gmail.com>
2023-11-20 21:00:29 +0200

    Removed the .idea folder

---
+++ lesson1/scripts/main.js
@@ -0,0 +1,15 @@
+// number
+let n1;
+n1 = 123.12334324;
+//string
+let s1 = 'hello okten 123';
+s1 = "qiweuyu gkg js agjdashb";
+s1 = ` ga jhgsa jhgajs hg`;
+console.log(s1);
+// boolean
+
+// > < >= <= == != === !==
+let b1 = 5 !== 6;
+console.log(b1);
+
+
