5230630c3b3d68229ff335d96cdb1e08ab08d80c
GrayHead <ss.zhuravlov@gmail.com>
2023-11-20 21:00:44 +0200

    iinit

--- lesson1/scripts/main.js
+++ lesson1/scripts/main.js
@@ -1,15 +1,115 @@
-// number
-let n1;
-n1 = 123.12334324;
-//string
-let s1 = 'hello okten 123';
-s1 = "qiweuyu gkg js agjdashb";
-s1 = ` ga jhgsa jhgajs hg`;
-console.log(s1);
-// boolean
+// // number
+// let n1;
+// n1 = 123.12334324;
+// //string
+// let s1 = 'hello okten 123';
+// s1 = "qiweuyu gkg js agjdashb";
+// s1 = ` ga jhgsa jhgajs hg`;
+// console.log(s1);
+// // boolean
+//
+// // > < >= <= == != === !==
+// let b1 = 5 !== 6;
+// console.log(b1);
+//
+// // let  ageOfMyFavoriteDog
+// let bool = 5 == '5'; // '5' == '5'-> 0091 == 0091
+// console.log(bool);
+//
+// bool = 100 === '100';
+// console.log(bool);
 
-// > < >= <= == != === !==
-let b1 = 5 !== 6;
-console.log(b1);
+// let x = null;
+//
+//
+// console.log(typeof x);
+//
+// // number
+// // string
+// // boolean
+// // undefined
+// // null
+// // NaN
+// // Symbol
+// // object [] {}
+// // console.log(typeof NaN);
+// // console.log(100 / 'asdsa');
+
+// let a = 10;
+// let b = 20;
+//
+// console.log(a + b); // - / * %
+// console.log(12 % 3);
+//
+// let str1 = 'hello';
+// let str2 = 'okten';
+// console.log(str1 + ' ' + str2 + '!!!');
+// console.log(`${str1} ${str2} !!!`);
+//
+// console.log(str1 - str2);
+//
+// let ss1 = '100';
+// let ss2 = '200';
+// console.log(ss1 - ss2);
+// console.log(ss1 + ss2);
+
+
+// n to s
+// let x = 100;
+// console.log(x + '');
+// console.log(300 + '');
+// console.log('' + 100 * 200);
+
+
+// S TO N
+// let ns = '100500';
+// let nn = +ns;
+// console.log(nn);
+// let number = parseInt('100500asdasdsa');
+// console.log(number);
+
+console.log(true + true);
+console.log(true + false);
+
+console.log(100 + true);
+console.log('100' / false);
+
+// b to n
+console.log(+true);
+console.log(+false);
+
+// n to b
+console.log(!!100);
+console.log(!!-100);
+console.log(!!'asdasd');
+console.log('-----------');
+console.log(!!0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+
+console.log(!!-Infinity);
+console.log(!!Infinity);
+
+
+
+
+
+
+
+git rm .idea/ -r --cached
+git add -u .idea/
+git commit -m "Removed the .idea folder"
+
+
+
+
+
+
+
+
+
+
 
 
