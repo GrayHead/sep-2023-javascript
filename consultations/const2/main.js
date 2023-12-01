8ac5411a504845a5bb13fc18f73eabe0f9511337
sprocivjs <sprocivjs@gmail.com>
2023-12-01 19:34:14 +0200

    add const #2

---
+++ consultations/const2/main.js
@@ -0,0 +1,237 @@
+console.log('TEST');
+let cars = [
+    {make: "Toyota", model: "Camry", year: 2022, color: "Blue", price: 25000},
+    {make: "Honda", model: "Civic", year: 2021, color: "Red", price: 22000},
+    {make: "Ford", model: "Mustang", year: 2023, color: "Black", price: 40000},
+    {make: "Chevrolet", model: "Silverado", year: 2022, color: "Gray", price: 35000},
+    {make: "Nissan", model: "Altima", year: 2021, color: "White", price: 23000},
+    {make: "BMW", model: "X5", year: 2022, color: "Silver", price: 60000},
+    {make: "Mercedes-Benz", model: "C-Class", year: 2023, color: "Black", price: 50000},
+    {make: "Audi", model: "A4", year: 2022, color: "Blue", price: 45000},
+    {make: "Jeep", model: "Wrangler", year: 2022, color: "Green", price: 35000},
+    {make: "Lexus", model: "RX", year: 2023, color: "Champagne", price: 55000}
+];
+let products = [
+    {name: "Laptop", price: 1000, category: "Electronics"},
+    {name: "T-shirt", price: 20, category: "Clothing"},
+    {name: "Book", price: 15, category: "Books"},
+    {name: "Shoes", price: 50, category: "Footwear"},
+    {name: "Headphones", price: 80, category: "Electronics"},
+    {name: "Backpack", price: 40, category: "Accessories"},
+    {name: "Guitar", price: 300, category: "Musical Instruments"},
+    {name: "Watch", price: 150, category: "Accessories"},
+    {name: "Cookware Set", price: 200, category: "Kitchenware"},
+    {name: "Bicycle", price: 400, category: "Sports & Outdoors"}
+];
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js'],
+
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+let users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    }
+];
+
+function FnIn(array){
+    for (const item of array) {
+        // console.log(item);
+        // if(item.make === 'Audi' || item.make === 'BMW'){
+            for (const key in item) {
+                // console.log(key)
+                document.write(`<div> 
+                            <h2 style="color: red">${key}: 
+                    <span style="color: cadetblue">${item[key]}</span></h2></div>`)
+            }
+            createHr('orange')
+        // }
+    }
+}
+FnIn(cars);
+createHr('red');
+FnIn(products);
+createHr('green');
+FnIn(coursesArray);
+
+function createHr(color){
+    document.write(`<hr style="border: 2px solid ${color}">`)
+}
+
+// function sum (a,b){
+//     return a + b;
+// }
+// let res = sum(10,20);
+// console.log(res)
+//
+//
+//
+// function dil (a,b){
+//     return a / b;
+// }
+//
+// let res1 = dil(15, 3)
+// console.log(res1)
+//
+// let result = res + res1;
+// console.log(result);
+
+
+
