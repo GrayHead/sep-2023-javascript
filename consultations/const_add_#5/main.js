8be7edba6bb21838f67a468944444b074bc84071
sprocivjs <sprocivjs@gmail.com>
2023-12-05 20:01:46 +0200

    add const add js #5

---
+++ consultations/const_add_#5/main.js
@@ -0,0 +1,22 @@
+console.log('CONST ADD #5');
+console.log('-------------------------');
+
+
+const arrow = (a = 10, b = 20) => a + b;
+
+let res = arrow();
+console.log(res)
+
+// console.log(name)
+let name = 'Vasia'
+
+let arr = (array) => {
+    console.log(array)
+}
+
+arr('test');
+
+
+
+
+
